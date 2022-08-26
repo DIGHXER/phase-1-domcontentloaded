d32029dd5e6c4e7a8d968c7c4634b7b61641bf8d
douglas <douglasmwangi94@gmail.com>
2022-08-26 21:10:55 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    const r = document.getElementById(`text`)
+    r.innerHTML = ``;
+
+    r.appendChild(document.createTextNode("This is really cool!"))
+  
+})
